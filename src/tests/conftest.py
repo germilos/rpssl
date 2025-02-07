b2584643f1372b4d305f087a75f6bb670ac688de
Milos Gerun <gerun@quantlabs.rs>
2025-02-07 13:17:46 +0100

    removed unused method

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -10,10 +10,3 @@ def client() -> TestClient:
     Fixture that returns an unauthenticated API Client
     """
     return TestClient(app)
-
-
-def pytest_sessionstart(session):  # noqa
-    """
-    Called after the Session object has been created and
-    before performing collection and entering the run test loop.
-    """
